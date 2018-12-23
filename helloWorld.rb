9f5741424a069ad45a5ed00ed7804a4899d05670
Robert Kenney <rakenney@ymail.com>
2018-12-23 11:05:35 -0500

    init

---
+++ helloWorld.rb
@@ -0,0 +1,19 @@
+#puts = put string 
+puts "Hello World"
+
+#p = print
+p "Hello World"
+
+#print = print sring without a new space (\n)
+print "Hello World"
+
+# variable is an address 
+greeting = "Hello World"
+puts greeting
+
+#def = short for define 
+def say_hello(thing_to_say)
+    puts thing_to_say
+end 
+
+say_hello "Hello Robert"
---
+++ strings.rb
@@ -0,0 +1,7 @@
+# String concatenation 
+first_name = "Robert"
+last_name = "Kenney"
+puts first_name + " " + last_name
+
+# String interpolation 
+puts "My first name is #{first_name} and my last name is #{last_name}."
