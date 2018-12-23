7c160e8b07c53644bfa3acf4b78c9cbc14af62c3
Robert Kenney <rakenney@ymail.com>
2018-12-23 11:09:19 -0500

    updates strings.rb

--- strings.rb
+++ strings.rb
@@ -3,5 +3,5 @@ first_name = "Robert"
 last_name = "Kenney"
 puts first_name + " " + last_name
 
-# String interpolation 
+# String interpolation - must use double quotes
 puts "My first name is #{first_name} and my last name is #{last_name}."
